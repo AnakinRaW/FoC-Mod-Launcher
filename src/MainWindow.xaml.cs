c2062a626c124c5bf8f62ff159262e7ee6082c88
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2019-04-21 20:56:52 +0200

    added double click

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Input;
+using FocLauncher.Mods;
 
 namespace FocLauncher
 {
@@ -13,6 +15,15 @@ namespace FocLauncher
         private void OpenAboutWindow(object sender, RoutedEventArgs e)
         {
             new AboutWindow().ShowDialog();
+        }
+
+        private void ListBox_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (((FrameworkElement)e.OriginalSource).DataContext is IMod)
+            {
+                var model = DataContext as MainWindowViewModel;
+                model?.LaunchCommand.Execute(null);
+            }
         }
     }
 }
--- src/MainWindowViewModel.cs
+++ src/MainWindowViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Input;
 using FocLauncher.Annotations;
 using FocLauncher.Game;
