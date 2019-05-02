7a109dd3dbe1ff3d296177a8e047f37ee7566d10
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2019-05-02 23:30:40 +0200

    added version info

--- src/FocLauncher/Dialogs/AboutWindow.xaml.cs
+++ src/FocLauncher/Dialogs/AboutWindow.xaml.cs
@@ -1,19 +1,59 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Windows.Navigation;
+using FocLauncher.Core.Properties;
+using FocLauncher.Theming;
 
 namespace FocLauncher.Core.Dialogs
 {
-    public partial class AboutWindow
+    public partial class AboutWindow : INotifyPropertyChanged
     {
+        private Version _launcherVersion;
+        private Version _themeVersion;
+
+        public Version LauncherVersion
+        {
+            get => _launcherVersion;
+            set
+            {
+                if (Equals(value, _launcherVersion)) return;
+                _launcherVersion = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Version ThemeVersion
+        {
+            get => _themeVersion;
+            set
+            {
+                if (Equals(value, _themeVersion)) return;
+                _themeVersion = value;
+                OnPropertyChanged();
+            }
+        }
+
         public AboutWindow()
         {
             InitializeComponent();
+            LauncherVersion = GetType().Assembly.GetName().Version;
+            ThemeVersion = typeof(ITheme).Assembly.GetName().Version;
         }
 
         private void OpenLicenseSite(object sender, RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
