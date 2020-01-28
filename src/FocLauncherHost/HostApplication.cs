f633d518b1ef7812b59e95657e408934865a8b7d
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2020-01-28 20:30:26 +0100

    deleted stuff

--- src/FocLauncherHost/HostApplication.cs
+++ src/FocLauncherHost/HostApplication.cs
@@ -14,6 +14,9 @@ namespace FocLauncherHost
 {
     public class HostApplication : Application
     {
+        // TODO: This should be a flexible server, not the final
+        public const string ServerUrl = "https://raw.githubusercontent.com/AnakinSklavenwalker/FoC-Mod-Launcher/";
+
         static HostApplication()
         {
             // Since FocLauncher.Threading.dll and Microsoft.VisualStudio.Utilities.dll are used by the WaitWindow AppDomain we need to have them on disk
@@ -22,47 +25,12 @@ namespace FocLauncherHost
             AssemblyExtractor.WriteNecessaryAssembliesToDisk(LauncherConstants.ApplicationBasePath, 
                 "FocLauncher.Threading.dll", "Microsoft.VisualStudio.Utilities.dll");
         }
-
-        // TODO: This should be a flexible server, not the final
-        public const string ServerUrl = "https://raw.githubusercontent.com/AnakinSklavenwalker/FoC-Mod-Launcher/";
-
-
-
-        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
-        private readonly ConnectionManager _connectionManager;
-
-        public HostApplication()
-        {
-            _connectionManager = ConnectionManager.Instance;
-        }
-
-        protected override /*async*/ void OnStartup(StartupEventArgs e)
+        
+        protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-
             MainWindow = new SplashScreen();
             PrepareAndUpdateLauncherAsync().Forget();
-
-            //await AssemblyExtractor.WriteNecessaryAssembliesToDiskAsync(LauncherConstants.ApplicationBasePath, "FocLauncher.dll", "FocLauncher.Theming.dll");
-
-            //await new UpdateManager().CheckAndPerformUpdateAsync();
-
-            //var actionQueue = new Queue<Func<Task>>();
-
-            //var launcherUpdater = new LauncherUpdater();
-            //await CheckForUpdate(launcherUpdater, actionQueue);
-            //var themeUpdater = new ThemeUpdater();
-            //await CheckForUpdate(themeUpdater,  actionQueue);
-            
-            //if (!actionQueue.Any())
-            //{
-            //    await WaitForMainWindow();
-            //    await Current.Dispatcher.Invoke(async () => await UpdateAsync(actionQueue), DispatcherPriority.Background);
-            //}
-
-            //MainWindow?.Dispatcher.InvokeShutdown();
-            //Dispatcher.InvokeShutdown();
-            //Shutdown();
         }
 
         internal async Task PrepareAndUpdateLauncherAsync()
@@ -108,86 +76,6 @@ namespace FocLauncherHost
                 await Task.Delay(500);
                 await splashScreen.HideAnimationAsync();
             }
-        }
-
-
-        private async Task CheckForUpdate(AssemblyUpdater updater, Queue<Func<Task>> actionQueue)
-        {
-            var hasConnection = await _connectionManager.CheckConnectionAsync();
-
-            var currentVersion = updater.CurrentVersion;
-
-            //TODO: Extract always when the embedded version is higher
-            if (!hasConnection && currentVersion == null)
-            {
-                actionQueue.Enqueue(async () => await AssemblyExtractor.WriteNecessaryAssembliesToDiskAsync(LauncherConstants.ApplicationBasePath, updater.AssemblyName));
-                //actionQueue.Enqueue(async () => await Task.Run(() => ResourceExtractor.ExtractAssembly(LauncherConstants.ApplicationBasePath, updater.AssemblyName)));
-                return;
-            }
-
-            // TODO: Async
-            var latestVersion = updater.LatestVersion;
-            if (currentVersion == null || latestVersion > currentVersion)
-                actionQueue.Enqueue(updater.Update);
-        }
-        
-        private async Task UpdateAsync(IEnumerable<Func<Task>> actions)
-        {
-            await ThreadHelper.JoinableTaskFactory.RunAsync(async () =>
-            {
-                var data = new WaitDialogProgressData("Please wait while the launcher is loading an update.", "Updating....", null, true);
-
-
-                var s = WaitDialogFactory.Instance.StartWaitDialog("123", data, TimeSpan.FromSeconds(2));
-                try
-                {
-                    foreach (var func in actions)
-                        await func();
-
-                    await Task.Delay(50000, s.UserCancellationToken);
-                }
-                catch (TaskCanceledException)
-                {
-                }
-                finally
-                {
-                    s.Dispose();
-                }
-            });
-
-
-            //var twd = WaitDialogServiceWrapper.CreateInstance();
-            //bool cancelled;
-            //twd.StartWaitDialog("FoC Launcher", "Please wait while the launcher is loading an update.", "Updating....",
-            //    true, 2, true, _cancellationTokenSource);
-            //try
-            //{
-            //    //await Task.Delay(5000, _cancellationTokenSource.Token);
-            //    if (_cancellationTokenSource.IsCancellationRequested)
-            //    {
-            //    }
-
-            //    //foreach (var func in actionQueue) 
-            //    //    await func();
-            //}
-            //catch (TaskCanceledException)
-            //{
-            //}
-            //finally
-            //{
-            //    twd.EndWaitDialog(out cancelled);
-            //}
-
-            //if (cancelled)
-            //    return;
-
-        }
-
-        private static async Task WaitForMainWindow()
-        {
-            if (Current.MainWindow != null && Current.MainWindow.IsVisible)
-                return;
-            await Task.Delay(100);
         }
     }
 }
