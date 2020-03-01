3e6e1e8c43ee06bd590749a53137bbeb3be8303f
Anakin Sklavenwalker <anakin@gmx-topmail.de>
2020-03-02 00:11:09 +0100

    added elevator project

---
+++ src/TaskBasedUpdater.Elevator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TaskBasedUpdater.Elevator")]
+[assembly: AssemblyDescription("Command Line Tool to create a process with administrator rights")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Republic at War Modding Team")]
+[assembly: AssemblyProduct("TaskBasedUpdater.Elevator")]
+[assembly: AssemblyCopyright("Copyright © 2020 Anakin Sklavenwalker")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b10160b6-ed51-48b7-a45d-637157d656ab")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
