a9a62243ae40e210df86de1ed37cef435172f9d1
AnakinSklavenwalker <anakin@gmx-topmail.de>
2020-03-29 23:37:55 +0200

    update dependencies

--- tools/MetadataCreator/Program.cs
+++ tools/MetadataCreator/Program.cs
@@ -8,7 +8,6 @@ namespace MetadataCreator
         static void Main(string[] args)
         {
             var options = (Parser.Default.ParseArguments<LaunchOptions>(args) as Parsed<LaunchOptions>)?.Value;
-
             Console.ReadKey();
         }
     }
