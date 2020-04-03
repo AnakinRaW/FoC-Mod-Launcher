5e299b6622b5fa4d7f84ac987c7ae2f3ba681f22
AnakinSklavenwalker <anakin@gmx-topmail.de>
2020-04-04 00:50:12 +0200

    edited release creation pipeline

--- compileRelease.bat
+++ compileRelease.bat
@@ -1,16 +1,27 @@
 @echo off
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" FocLauncher.sln /t:Rebuild /p:Configuration=Release /p:Platform="Any CPU"
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" FocLauncher.sln /t:Build /p:Configuration=Release /p:Platform="Any CPU" /m
 set BUILD_STATUS=%ERRORLEVEL% 
 if %BUILD_STATUS%==0 goto buildSuccess 
-if not %BUILD_STATUS%==0 goto buildFail 
+if not %BUILD_STATUS%==0 goto fail 
  
 :buildSuccess 
 echo build was successful
 pause 
-exit /b 0 
+goto createMetadata
+ 
+ 
+:createMetadata
+MetadataCreator.exe -o gen
+set CREATOR_STATUS=%ERRORLEVEL% 
+if %BUILD_STATUS%==0 goto createSuccess
+if not %CREATOR_STATUS%==0 goto fail
+
+:createSuccess
+pause 
+exit /b 0
 
 
-:buildFail 
+:fail 
 echo operation failed...
 pause 
 exit /b 1 
