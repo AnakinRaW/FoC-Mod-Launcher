65a382a272d61c9bc91ed8e7f1b74f1800a297a2
AnakinSklavenwalker <anakin@gmx-topmail.de>
2020-04-28 22:19:45 +0200

    status bar hover fix

---
+++ src/FocLauncher/Controls/StatusBarButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace FocLauncher.Controls
+{
+    internal class StatusBarButton : Button
+    {
+        static StatusBarButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(StatusBarButton), new FrameworkPropertyMetadata(typeof(StatusBarButton)));
+        }
+    }
+}
