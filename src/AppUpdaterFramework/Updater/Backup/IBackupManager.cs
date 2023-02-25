c4e4830c2fbc760192bd02d53100d999b7f157b4
AnakinRaW <anakin@gmx-topmail.de>
2023-02-26 00:16:00 +0100

    start backup impl.

---
+++ src/AppUpdaterFramework/Updater/Backup/IBackupManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AnakinRaW.AppUpaterFramework.Metadata.Component;
+
+namespace AnakinRaW.AppUpaterFramework.Updater.Backup;
+
+internal interface IBackupManager
+{
+    void BackupComponent(IInstallableComponent component);
+
+    void RemoveBackups();
+}
+
+internal class BackupManager : IBackupManager
+{
+    public BackupManager(IServiceProvider serviceProvider)
+    {
+        
+    }
+
+    public void BackupComponent(IInstallableComponent component)
+    {
+    }
+
+    public void RemoveBackups()
+    {
+        
+    }
+}
