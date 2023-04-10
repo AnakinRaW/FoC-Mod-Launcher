62dc07e12a70a7dcafc429f335d765c4fd2c3fc4
AnakinSklavenwalker <anakin@gmx-topmail.de>
2023-04-10 18:48:32 +0200

    get component info from assemblies

--- src/FocLauncher/AssemblyInfo.cs
+++ src/FocLauncher/AssemblyInfo.cs
@@ -4,4 +4,4 @@ using AnakinRaW.AppUpdaterFramework.Attributes;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
 [assembly: UpdateComponent("FocLauncher.Executable", Name = "Foc Launcher Application")]
-[assembly: UpdateProduct("Forces of Corruption Game Launcher")]
+[assembly: UpdateProduct("Forces of Corruption Mod Launcher")]
