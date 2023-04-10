3bc345524b8d4914cbbf8a039eab083ea50cb7b8
AnakinRaW <anakin@gmx-topmail.de>
2023-04-10 14:29:53 +0200

    update code

--- src/FocLauncher/AssemblyInfo.cs
+++ src/FocLauncher/AssemblyInfo.cs
@@ -1,3 +1,7 @@
 using System.Windows;
+using AnakinRaW.AppUpdaterFramework.Attributes;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+
+[assembly: UpdateComponent("FocLauncher.Executable", Name = "Foc Launcher Application")]
+[assembly: UpdateProduct("Forces of Corruption Game Launcher")]
